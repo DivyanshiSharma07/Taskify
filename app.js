ddfb2002e3068532f54a265f5af1df3a88591514
Divyanshi Sharma <mac@Divyanshi-Sharma.local>
2023-06-30 01:37:08 +0530

    vercel change

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const _ = require("lodash");
 
 
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -149,7 +150,7 @@ app.get("/:customListName", async function(req, res) {
 });
 
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+app.listen(PORT, function () {
+  console.log("Server started on port ${PORT}");
 });
 }).catch((err) => console.log(err))
